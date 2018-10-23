08558e07efd8fdb620b6a5aaf7ee2955064f07d4
amb seny <adrifre7@hotmail.com>
2018-10-23 16:59:47 +0200

    first commit

---
+++ TetrisMobKata.Test/GameTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TetrisMobKata.Test
+{
+    [TestClass]
+    public class GameTest
+    {
+        [TestMethod]
+        public void GivenIsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ TetrisMobKata.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TetrisMobKata.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TetrisMobKata.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0630bc48-fb96-494c-9561-38de63d1dccd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
